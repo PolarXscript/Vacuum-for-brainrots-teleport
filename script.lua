ae709da5bd149f08b379908fd12fa37f4fbee850
X [Script] <deofivem@gmail.com>
2026-04-02 18:01:03 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -6,7 +6,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 -- =====================
 --    KEY SYSTEM MANUAL
 -- =====================
-local CORRECT_KEY  = "X free2"
+local CORRECT_KEY  = "X script Free1"
 local keyPassed    = false
 
 -- Bikin GUI key manual
